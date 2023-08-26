99bb63df077b741a05c52dd9e9151828486cacdf
kristhyanmatos <kristhyandematosmaia@gmail.com>
2023-08-26 17:55:37 -0300

    refactor: scroll

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'portfolio.dart';
 
@@ -9,14 +8,6 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kristhyan de Matos Maia',
-      scrollBehavior: const MaterialScrollBehavior().copyWith(
-        dragDevices: {
-          PointerDeviceKind.mouse,
-          PointerDeviceKind.touch,
-          PointerDeviceKind.stylus,
-          PointerDeviceKind.unknown
-        },
-      ),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
